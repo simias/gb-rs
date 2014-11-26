afbbf8ffaaf8c58b885434945a06a212635c720d
Lionel Flandrin <lionel@svkt.org>
2014-11-27 00:30:47 +0100

    Implement timer for accurate emulation speed

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,9 @@
 
 extern crate sdl2;
 
+use std::io::Timer;
+use std::time::Duration;
+
 mod cpu;
 mod io;
 mod gpu;
@@ -45,7 +48,37 @@ fn main() {
 
     cpu.reset();
 
+    // In order to synchronize the emulation speed with the wall clock
+    // we need to wait at some point so that we don't go too
+    // fast. Waiting between each cycle would mean a storm of syscalls
+    // back and forth between the kernel and us, so instead we execute
+    // instructions in batches of GRANULARITY cycles and then sleep
+    // for a while. If the GRANULARITY value is too low we'll go to
+    // sleep very often which will have poor performance. If it's too
+    // high it might look like the emulation is stuttering.
+    let mut timer = match Timer::new() {
+        Ok(t)  => t,
+        Err(e) => panic!("Couldn't create timer: {}", e),
+    };
+
+    let batch_duration = Duration::nanoseconds(GRANULARITY * (1_000_000_000 /
+                                                              SYSCLK_FREQ));
+
+    let tick = timer.periodic(batch_duration);
+
     loop {
-        cpu.step();
+        for _ in range(0, GRANULARITY) {
+            cpu.step();
+        }
+        // Sleep until next batch cycle
+        tick.recv();
     }
 }
+
+/// Number of instructions executed between sleeps (i.e. giving the
+/// hand back to the scheduler). Low values increase CPU usage and can
+/// result in poor performance, high values will cause stuttering.
+const GRANULARITY:      i64 = 0x10000;
+
+/// Gameboy sysclk frequency: 4.19Mhz
+const SYSCLK_FREQ:      i64 = 0x400000;
